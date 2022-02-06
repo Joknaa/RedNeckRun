ec5267d5e1ee5c596b7aa73b4cbea2f4364c2bf4
Joknaa <54961980+Joknaa@users.noreply.github.com>
2022-02-06 16:53:11 +0100

    Created movement script
    Created the movement script where the player moves continuously forward, and moves sideways according to the player's swipes on the screen.

---
+++ RedNeckRun/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    private GameObject playerCharacter;
+    [SerializeField] private float distanceFromPlayer;
+    void Start() {
+        playerCharacter = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    void Update() {
+        Vector3 position = transform.position;
+        
+        transform.position = new Vector3(position.x, position.y, playerCharacter.transform.position.z - distanceFromPlayer);
+    }
+}
---
+++ RedNeckRun/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+    private Rigidbody _rigidbody;
+    private Vector2 startTouchPosition, endTouchPosition;
+
+    [SerializeField] private int moveSpeed;
+    [SerializeField] private int sidewaysMove;
+
+    void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        SwipeHorizontally();
+    }
+
+    private void SwipeHorizontally() {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            startTouchPosition = Input.GetTouch(0).position;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+            endTouchPosition = Input.GetTouch(0).position;
+
+            Vector3 currentPosition = transform.position;
+            if ((endTouchPosition.x < startTouchPosition.x))
+                transform.position = new Vector3(currentPosition.x - sidewaysMove, currentPosition.y, currentPosition.z);
+            if ((endTouchPosition.x > startTouchPosition.x))
+                transform.position = new Vector3(currentPosition.x + sidewaysMove, currentPosition.y, currentPosition.z);
+            
+        }
+    }
+}
